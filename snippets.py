b50f7f6cb9c609c18c37d772a4085702226bc85e
avargas94 <andrewvargas6933@gmail.com>
2018-03-10 19:41:01 +0000

    Snippet

---
+++ snippets.py
@@ -0,0 +1,47 @@
+import logging, argparse
+
+#Setting the log output file, and the log level
+
+#DEBUG: Detailed information, typically of interest only when diagnosing problems.
+#INFO: Confirmation that things are working as expected.
+#WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+#ERROR: Due to a more serious problem, the software has not been able to perform some function.
+#CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippet with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def get(name):
+    """Retrieve the snippet with a given name.
+
+    If there is no such snippet, return '404: Snippet Not Found'.
+
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+    
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+
+    arguments = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
